51906f769d189552e577b8b18bc6df2ca8b88e0f
Juan <jpascualanaya@gmail.com>
2014-02-02 15:47:17 +0900

    Update config.sh

--- smrtanalysis-2.0.1/config.sh
+++ smrtanalysis-2.0.1/config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-. /net/eichler/vol20/projects/pacbio/opt/smrtanalysis-2.0.1/etc/setup.sh
+. /home/champi/Documents/hagfish/pacbio/pacbio-protocols4bas.h5/smrtanalysis-2.0.1/etc/setup.sh
 
-. /etc/profile.d/modules.sh
-if test ! -z $MODULESHOME; then
-   module load modules modules-init modules-gs/prod modules-eichler/prod
-fi
+# . /etc/profile.d/modules.sh
+# if test ! -z $MODULESHOME; then
+#   module load modules modules-init modules-gs/prod modules-eichler/prod
+# fi
 
-module load cross_match/latest
-module load bedtools/latest
-export MAKEDIR=/net/eichler/vol4/home/jlhudd/projects/pacbio/smrtanalysis-2.1.0
-export VECTOR=/net/eichler/vol4/home/jlhudd/projects/pacbio/vector/vector.fasta
+# module load cross_match/latest
+# module load bedtools/latest
+export MAKEDIR=/home/champi/Documents/hagfish/pacbio/pacbio-protocols4bas.h5/smrtanalysis-2.0.1
+export VECTOR=/home/champi/Documents/hagfish/pacbio/pCC1BAC-HindIII.fasta
