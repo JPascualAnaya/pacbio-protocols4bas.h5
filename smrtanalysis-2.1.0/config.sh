6d0d0606a210b9bd4b48cba7d3fba5d988889858
Juan <jpascualanaya@gmail.com>
2014-02-07 13:48:26 +0900

    Update config.sh

--- smrtanalysis-2.1.0/config.sh
+++ smrtanalysis-2.1.0/config.sh
@@ -9,5 +9,5 @@
 
 # module load cross_match/latest
 # module load bedtools/latest
-export MAKEDIR=/home/champi/Documents/hagfish/pacbio/pacbio-protocols4bas.h5_b/smrtanalysis-2.1.0
+export MAKEDIR=/home/champi/Documents/hagfish/pacbio/pacbio-protocols4bas.h5/smrtanalysis-2.1.0
 export VECTOR=/home/champi/Documents/hagfish/pacbio/pCC1BAC-HindIII.fasta
